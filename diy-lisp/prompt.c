7f14ee641cf5eff43a7fd9283da4949f601f0359
Sean Lewis <scl63@drexel.edu>
2022-10-25 21:47:07 -0400

    added evaluation

--- diy-lisp/prompt.c
+++ diy-lisp/prompt.c
@@ -23,6 +23,7 @@ functions normally. Otherwise, only code after the #endif statement is run.
 #ifdef _WIN32
 #include <string.h>
 
+/* Declare a buffer for user input of size 2048. */
 static char buffer[2048];
 
 /* Windows-only readline function */
@@ -43,8 +44,37 @@ void add_history(char* unused) {}
 #include <editline/readline.h>
 #endif
 
-// Declare a buffer for user input of size 2048.
-//static char input[2048];
+/* Use operator strong to see which operation to perform */
+long eval_op(long x, char* op, long y){
+  if (strcmp(op, "+") == 0) { return x + y; }
+  if (strcmp(op, "-") == 0) { return x - y; }
+  if (strcmp(op, "*") == 0) { return x * y; }
+  if (strcmp(op, "/") == 0) { return x / y; }
+  return 0;
+}
+
+long eval(mpc_ast_t* t) {
+
+  /* If tagged as number return it directly. */
+  if (strstr(t->tag, "number")) {
+    return atoi(t->contents);
+  }
+  /* The operator is always second child. */
+  char* op = t->children[1]->contents;
+
+  /* We store the third child in `x` */
+  long x = eval(t->children[2]);
+
+  /* Iterate the remaining children and combining. */
+
+  int i = 3;
+  while (strstr(t->children[i]->tag, "expr")) {
+    x = eval_op(x, op, eval(t->children[i]));
+    i++;
+  }
+  
+  return x;
+}
 
 int main(int argc, char** argv) {
   /* Create Some Parsers */
@@ -78,18 +108,22 @@ int main(int argc, char** argv) {
     /* Attempt to Parse the user Input */
     mpc_result_t r;
     if (mpc_parse("<stdin>", input, Sartoris, &r)) {
-      /* On Success Print the AST */
-      mpc_ast_print(r.output);
+      /* On Success Print the evaluated output */
+      long result = eval(r.output);
+      printf("%li\n", result);
       mpc_ast_delete(r.output);
+      
     } else {
       /* Otherwise Print the Error */
       mpc_err_print(r.error);
       mpc_err_delete(r.error);
     }
-
+    
     /* Free retrieved input */
     free(input);
   }
+  
   mpc_cleanup(4, Number, Operator, Expr, Sartoris);
+  
   return 0;
 }
