f8db69137e10d6eaa1c00cfd1242257c8830360a
Sean Lewis <scl63@drexel.edu>
2023-10-02 22:36:17 -0400

    *

--- basics-cpp/stackHeap.cpp
+++ basics-cpp/stackHeap.cpp
@@ -1,22 +1,101 @@
 #include <iostream>
 
-int main()
+void psi(int & i)
 {
-    int val = 5;         // Save val data in STACK
-    std::cout << val << " From the STACK\n";
-    int* hval = new int; // Save hval data in HEAP
-    *hval = 5;
-    std::cout << hval[0] << " From the HEAP\n";
+    // Print stack information of integer input.
+    // Must pass pointer (*) or reference (&) in, otherwise 
+    // input ints are simply copied into the same memory address 
+    // over and over.
+    // REMEMBER: functions will by default COPY the input vars.
+    std::cout << &i << " " << i << " " << sizeof(i) << "\n";
+}
 
-    int a[5];
-    a[3] = 10;            // Array on the STACK
-    std::cout << a[3] << " From a STACK array\n";
-    int* ha = new int[5]; // Array on the HEAP
-    ha[3] = 13;
-    std::cout << ha[3] << " From a HEAP array\n";
-    
-    delete hval;          // Must DELETE memory from HEAP
-    delete ha;
+// Use template to make the arrays truely dyanmic!
+template <class T>
+class DynamicArray
+{
+    size_t m_size; // need to initialize size, C++ only knows where
+                   // where arrays begin, not how long they are.
+    T *  m_arr;
+public:
+    // Constructor acquires the resource.
+    DynamicArray(size_t size)
+        : m_size(size)
+        , m_arr (new T[size]{})
+    {
+        // Do something whenever the array is constructed.
+        std::cout << "Array Constructor\n";
+    }
+    // Destructor
+    ~DynamicArray()
+    {
+        delete [] m_arr;
+        std::cout << "Array Destructor\n";
+    }
+
+    T get(size_t index) const
+    {
+        // takes an index and outputs array value 
+        // (cannot modify output array val so use const)
+        return m_arr[index];
+    }
+
+    void set(size_t index, T val)
+    {
+        // takes index and value andd sets arr element
+        // not const since we are modifying
+        m_arr[index] = val;
+    }
+
+    void print() const
+    {
+        // standard printing procedure for array
+        for (size_t i=0; i<m_size; i++)
+        {
+            std::cout << i << " " << m_arr[i] << "\n";
+        }
+    }
+    // Operator Overloading
+    // Hijack [] operator to allow for dynamic value setting
+    const T & operator [] (size_t index) const
+    {
+        return m_arr[index];
+    }
+    T & operator [] (size_t index)
+    {
+        return m_arr[index];
+    }
+};
+
+int main(int argc, char * argv[])
+{
+    std::cout << "Stack Allocated Memory\n";
+    int a = 10;
+    int b = 25;
+
+    std::cout << "Single ints:\n" "Address   Value   Size\n";
+    psi(a);
+    psi(b);
+
+    std::cout << "\nContiguous Array:\n" "Address   Value   Size\n";
+    int arr[5] = {}; // = {} gives default values (0) for array.
+    for (size_t i=0; i<10; i++)
+    {
+        psi(arr[i]);
+    }
+
+    int* pa = &a;
+    int* pb = &b;
+
+    *pa = 17;
+    // Equivaluent to *(&a) = 17;
+    std::cout << "Modify variable by accessing its pointer\n";
+    psi(a);
+
+    // We can also modify values by doing pointer math
+
+    *(pb-1) = 25;
+    // Equivalent to *pa = 25;
 
     // Stack Memory
     //      - Predefined size of memory (a few MB)
@@ -35,6 +114,29 @@ int main()
     //      - OS finds a contiguous chunk of memory and returns
     //        pointer to it. This is VERY EXPENSIVE.
 
-    // RAII
+    // RAII (Resource Acquisition is Initialization)
+        // Use class construction to build and maintain dynamic arrays
+        // Array stored in HEAP and auto releases memory on destruction.
+    
+    DynamicArray<int> myArray(5); // constructs
+    
+    myArray.set(2, 134);
+    myArray.set(4, 12);
+    myArray.print();
+
+    // RAII compliant class allows for dynamic typing!
+    DynamicArray<float> myFArray(5);
+    myFArray.set(3, 12.5);
+    myFArray.get(3);
+    myFArray.print();
+
+    // But what if I want to jjust set an array element on the fly?
+    // Operator Overloading.
+    myFArray[3] = 13.37;
+    myFArray.print();
+    // Pointers. Why Use Them?
+    //      1. Must use pointer for inheritance (Base * ptr = new Derived();)
+    //      2. Pass by value vs. by reference
+    //      3. Pointing to large data
     return 0;
 }
