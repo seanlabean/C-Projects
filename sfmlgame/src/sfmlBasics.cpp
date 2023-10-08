0503cbb64a76703995f06a1ec16e34a000f2a5a1
Sean Lewis <scl63@drexel.edu>
2023-10-08 09:04:48 -0400

    added animation; config file

--- sfmlgame/src/sfmlBasics.cpp
+++ sfmlgame/src/sfmlBasics.cpp
@@ -1,23 +1,65 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    // create window of size w*h pixels
+    // top left of window is (0,0) and bottom right is (w,h)
+    const int wWidth = 1000;
+    const int wHeight  = 600;
+    sf::RenderWindow window(sf::VideoMode(wWidth, wHeight), "SFML works!");
+    window.setFramerateLimit(60);
+    
+    // make a shape, set its size, color, position, velocity
+    sf::CircleShape circle(100.f);
+    circle.setFillColor(sf::Color::Green);
+    circle.setPosition(200.f, 200.f);
+    float circleMoveSpeed = 0.01f;
 
+    // start Font instance, load Font and check for success
+    sf::Font myFont;
+
+    if(!myFont.loadFromFile("fonts/MiraiSeu.ttf"))
+    {
+        std::cerr << "Could not load font!\n";
+        exit(-1);
+    }
+
+    // place text
+    sf::Text text("SAMPLE TEXT sample text", myFont, 36);
+    std::cout << (float)text.getCharacterSize();
+    text.setPosition(0, wHeight-(float)text.getCharacterSize());
+
+    // main loop
     while (window.isOpen())
     {
+        // event loop
         sf::Event event;
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
+            {
                 window.close();
+            }
+            if (event.type == sf::Event::KeyPressed)
+            {
+                // print code of key that was pressed
+                std::cout << "Key pressed with code = " << event.key.code << "\n";
+                if (event.key.code == sf::Keyboard::X)
+                {
+                    // reverse the shape velocity direction here.
+                }
+            }
         }
 
+        float sx = 0.5f;
+        float sy = 0.5f;
+        circle.setPosition(circle.getPosition().x + sx, circle.getPosition().y + sy);
+
         window.clear();
-        window.draw(shape);
-        window.display();
+        window.draw(circle);
+        window.draw(text);
+        window.display();       // controls OpenGL display buffers
     }
 
     return 0;
