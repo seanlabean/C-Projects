e966c428bc90356fd345a7e4ee5269940f0e0b23
Sean Lewis <scl63@drexel.edu>
2023-10-10 21:26:47 -0400

    working myself into a corner...

--- sfmlgame/README.md
+++ sfmlgame/README.md
@@ -1,3 +1,11 @@
 # Learning C++ Through Game Development.
 
 This repo tracks my progress of learning C++ and the fundamentals of game development by following the Memorial University [COMP 4300 course](https://www.youtube.com/watch?v=S7lXSihz0ac&list=PL_xRyXins848nDj2v-TJYahzvs-XW9sVV) lead by Professor David Churchill.
+
+## Assignment 1: Shapes That Bounce
+
+My first test as a newly born C++ programmer is to make a "game" consisting of bouncing shapes. Though, the actual requirements are a bit more strict and well defined:
+
+1. The Shapes are either Circles or Rectangles and their data (type, name, color, initial position, velocity) are read in from `config`.
+2. Shapes must "bounce" off of the walls of the game window.
+3. Shape names must be displayed at the center of each Shape.
--- sfmlgame/src/sfmlBasics.cpp
+++ sfmlgame/src/sfmlBasics.cpp
@@ -1,15 +1,52 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <fstream>
 
 class AShape
 {
 public:
     sf::CircleShape circle;
     sf::RectangleShape rect;
-    float speedX, speedY;
+    sf::Color color;
+    std::string name;
+    float px, py, vx, vy;
 };
+void readFromConfig(const std::string& filename)
+{
+    std::ifstream fin(filename);
+    std::string name, fontLoc, circleColor, rectColor;
+    int fontSize, fontColorR, fontColorG, fontColorB;
+    int circleColorR, circleColorG, circleColorB;
+    int rectColorR, rectColorG, rectColorB;
+    int wWidth, wHeight; 
+    float circleX, circleY, rectX, rectY;
+    float circleVx, circleVy, rectVx, rectVy;
+    float circleR, rectW, rectH;
+
+    while (fin >> name)
+    {
+        if (name=="Window")
+        {
+            fin >> wWidth >> wHeight;
+        } else if (name=="Font")
+        {
+            fin >> fontLoc >> fontSize >> fontColorR >> fontColorG >> fontColorB;
+        } else if (name=="Circle")
+        {
+            fin >> circleColor >> circleX >> circleY >> circleVx >> circleVy >> circleColorR >> circleColorG >> circleColorB >> circleR;
+        }else if (name=="Rectangle")
+        {
+            fin >> rectColor >> rectX >> rectY >> rectVx >> rectVy >> rectColorR >> rectColorG >> rectColorB >> rectW >> rectH;
+        }
+    }
+    std::cout << name << " " << wWidth << " " << wHeight << "\n";
+    std::cout << fontLoc << " " << fontSize << "\n";
+    std::cout << rectColor << " " << rectW << " " << rectH << "\n";
+}
 int main()
 {
+    readFromConfig("config");
+
     // create window of size w*h pixels
     // top left of window is (0,0) and bottom right is (w,h)
     const int wWidth = 1000;
