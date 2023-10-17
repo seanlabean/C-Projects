e250a41eee56ad02b5a8fc19952b0cfcb50bd2e8
Sean Lewis <scl63@drexel.edu>
2023-10-16 21:27:41 -0400

    ecs basics

--- basics-cpp/ecsBasics.cpp
+++ basics-cpp/ecsBasics.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 
+// Why are writing our own 2D vector class
+// (SFML already has Vector2f)? We can add other 
+// great functionality, and it's good to learn!
 class Vec2
 {
 public:
@@ -13,6 +16,42 @@ public:
         , y(yin)
     {
     }
+    // adding operator (create new vector)
+    Vec2 operator + (const Vec2& rhs) const
+    {
+        return Vec2(x + rhs.x, y + rhs.y);
+    }
+    // '+=' operator
+    void operator += (const Vec2& rhs)
+    {
+        x += rhs.x;
+        y += rhs.y;
+    }
+    // add operator (modifies one vector)
+    // method chaining returns reference to itself
+    Vec2& add(const Vec2& v)
+    {
+        x += v.x;
+        y += v.y;
+        return *this;
+    }
+    // What is chaining good for?
+    Vec2& scale(float s)
+    {
+        x *=s;
+        y *=s;
+        return *this;
+    }
+    Vec2& rotate(float deg)
+    {
+        return *this;
+    }
+
+    // Other good tools. Distance formula.
+    float dist(const Vec2& v) const
+    {
+        return sqrtf((v.x-x)*(v.x-x)+(v.y-y)*(v.y-y));
+    }
 };
 
 int main(int argc, char * argv[])
@@ -20,7 +59,27 @@ int main(int argc, char * argv[])
     Vec2 v1(100, 200);
     Vec2 v2(50, 75);
 
-    Vec2 v3(v1.x + v2.x, v1.y + v2.y);
-
+    // using Vec2 class '+' operator
+    Vec2 v3 = v1 + v2;
     std::cout << v3.x << " " << v3.y;
+
+    // another way to add
+    v1.add(v2);
+
+    v1 += v2;
+
+    Vec2 v3 (1000,2000);
+
+    // How to add v2 and v3 to v1?
+    // use "chained" add function
+    v1.add(v2).add(v3);
+
+    // Put all these chained functions together!
+    float s = 5.0;
+    v1.scale(s).rotate(30).add(v2);
+    // Can also call one at a time
+    v1.scale(s);
+    v1.rotate(30);
+
+    float dist = v1.dist(v2);
 }
