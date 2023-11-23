465355ff2280816d2f01fff28112dc635ff7d6c0
Sean Lewis <scl63@drexel.edu>
2023-11-22 22:24:23 -0500

    rewrite

--- kaching/kaching.c
+++ kaching/kaching.c
@@ -1,25 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
-int
-main()
-{
-	/* Init calculation floats, prompt and check user input */
-	float s = 0, t = 0, c = 0;
-	printf("Monthly Salary | $ ");
-	if(!scanf("%f", &s)) {
-		printf("Invalid format\n");
-		return 0;
-	}
-	/* get tick (per second pay increase) */
-	t = s / 20 / 8 / 60 / 60;
-	putchar('\n');
-	/* Print summed cash, step c by t */
-	do {
-		printf("\033[A$ %.2f\n",c);
-		sleep(1);
-		c+=t;
-	}while(t >= 0);
-	printf("Ended.\n");
-	return 0;
+int main() {
+    float s = 0, t = 0, c = 0;
+    char input[100];  
+
+    printf("Yearly Salary | $");
+
+    if (fgets(input, sizeof(input), stdin) == NULL) 
+    {
+        printf("Error reading input.\n");
+        return 1;
+    }
+    if (!(sscanf(input, "%f", &s) == 1)) 
+    {
+        printf("Invalid input. Please enter a valid float.\n");
+    } 
+    t = s / 12 / 20 / 8 / 60 / 60;
+    putchar('\n');
+
+    do {
+        printf("\r");
+        printf("\033[ARaked In: $%.2f\n\r", c); 
+        sleep(1);
+        c += t;
+    } while (c < t * 60 * 60 * 8);
+
+    printf("One day's pay.\n");
+
+    return 0;
 }
